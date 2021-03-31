48f053a43aaaf3c93f95b82a3261af17da5d2df7
golazin <abolfathig@student.douglascollege.ca>
2021-03-30 21:15:08 -0700

    template created

---
+++ CarParkManagementSystem/src/test/java/com/example/CarParkManagementSystem/CarParkManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarParkManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarParkManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
